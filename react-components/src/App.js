9a983a760ad37ea71b6e44c167a18a728d9f2577
ivan grimaldo <ivan.grimaldo@spreetail.com>
2022-08-11 11:07:55 -0500

    Modal component

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,5 +1,14 @@
+import { Modal } from "./Modal/Modal";
+import { useModal } from "./Modal/useModal";
+
 function App() {
-  return <>"Hello world"</>;
+  const { isOpen, toggle } = useModal();
+  return (
+    <>
+      <Modal isOpen={isOpen} toggle={toggle} children={"this works"}></Modal>
+      <button onClick={toggle}>Open Modal</button>
+    </>
+  );
 }
 
 export default App;
